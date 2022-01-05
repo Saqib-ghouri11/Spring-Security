ce6e7189a9492e63284d4899190a339c298363d0
Saqib-ghouri11 <39990883+Saqib-ghouri11@users.noreply.github.com>
2022-01-05 17:17:14 +0500

    custom-login-inMemoryUser with BCrypt password encoding and public apis(doesn't need to be authenticated)

--- spring-boot-security/src/main/java/com/spring/springbootsecurity/configs/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/spring/springbootsecurity/configs/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.spring.springbootsecurity.configs;
 
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,6 +18,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated().and().httpBasic();
+                .authorizeRequests()
+                .antMatchers("/pub/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication().withUser("saqib").password(getPasswordEncoder().encode("123456")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("ali").password(getPasswordEncoder().encode("12345")).roles("NORMAL");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    // the commented code below can be used with no password encoding and is not recommended in production.
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("saqib").password("123456").roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("ali").password("123456").roles("NORMAL");
+//    }
+//
+//    @Bean
+//    PasswordEncoder getPasswordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
 }
--- spring-boot-security/src/main/java/com/spring/springbootsecurity/controllers/rest/HomeController.java
+++ spring-boot-security/src/main/java/com/spring/springbootsecurity/controllers/rest/HomeController.java
@@ -1,13 +1,23 @@
 package com.spring.springbootsecurity.controllers.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pub")
 public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "Welcome to Spring Security.";
+        return "Welcome to Spring Security home.";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "Welcome to Spring Security login.";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "Welcome to Spring Security register.";
     }
 }
